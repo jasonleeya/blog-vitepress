378033e9547209c8cfeb7ca6d214d6f7b4cb66ef
LSJ <1615685977@qq.com>
2024-01-04 09:52:57 +0800

    主题色优化

--- .vitepress/theme/components/icons/IconLoading.vue
+++ .vitepress/theme/components/icons/IconLoading.vue
@@ -2,8 +2,8 @@
   <svg viewBox="0 0 100 20" xmlns="http://www.w3.org/2000/svg" width="800" height="160">
     <defs>
       <linearGradient id="gradient" x1="0" x2="0" y1="0" y2="1">
-        <stop offset="5%" stop-color="#1772f6"/>
-        <stop offset="95%" stop-color="#174281"/>
+        <stop offset="5%" stop-color="#42b883"/>
+        <stop offset="95%" stop-color="#35495E"/>
       </linearGradient>
       <pattern id="wave" x="0" y="0" width="120" height="20" patternUnits="userSpaceOnUse">
         <path id="wavePath" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z"  fill="url(#gradient)">
