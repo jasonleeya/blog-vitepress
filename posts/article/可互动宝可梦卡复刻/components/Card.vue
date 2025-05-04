377e5af53049ceafaf0438939d8c08da5c98e811
lsj <lisj@internal.ths.com.cn>
2025-05-04 13:23:52 +0800

    可互动宝可梦卡复刻

---
+++ posts/article/可互动宝可梦卡复刻/components/Card.vue
@@ -0,0 +1,152 @@
+<!--
+ * @Author: 李双江
+ * @Date: 2025/5/3 20:58:45
+ * @LastEditors: 李双江
+ * @LastEditTime: 2025/5/3 20:58:45
+ * @Description:
+-->
+<template>
+  <div class="card-wrapper">
+    <div class="card"
+         @mouseenter="handleMouseEnter"
+         @mousemove="handleMouseMove"
+         @mouseleave="handleMouseLeave"
+         @touchstart="handleMouseEnter"
+         @touchmove="handleMouseMove"
+         @touchend="handleMouseLeave"
+         ref="cardRef"
+         :style="{transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
+          transition: shouldTransition?'transform 0.2s ease-out!important':'',
+          '--img-url':`url(${props.imgUrl})`,
+          '--foil-img-url':`url(${props.foilUrl})`,
+          '--pointer-x': pointerX + 'px',
+          '--pointer-y': pointerY + 'px',
+          '--radial-size':isHover?'70%':'0',
+          }">
+      <div class="shine" :style="{opacity: isHover ? 1 : 0}"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {onMounted, PropType, ref} from "vue";
+
+const props = defineProps({
+  imgUrl: {
+    type: String as PropType<string>,
+    default: 'https://6c73-lsj97-9giu4cj4abdc0985-1256331827.tcb.qcloud.la/imgs/2025_05/49_hires.png',
+  },
+  foilUrl: {
+    type: String as PropType<string>,
+    default: 'https://6c73-lsj97-9giu4cj4abdc0985-1256331827.tcb.qcloud.la/imgs/2025_05/49_foil.webp',
+  }
+})
+
+const cardRef = ref()
+
+let cardLeft = 0
+let cardTop = 0
+const rotateX = ref(0)
+const rotateY = ref(0)
+const pointerX = ref(0)
+const pointerY = ref(0)
+const isHover = ref(false)
+
+onMounted(() => {
+  const bcr = cardRef.value.getBoundingClientRect()
+  cardLeft = bcr.left
+  cardTop = bcr.top
+})
+const shouldTransition = ref(false)
+const handleMouseEnter = () => {
+  shouldTransition.value = true
+  isHover.value = true
+}
+const handleMouseMove = (e: any) => {
+  let event = e
+  if(e.targetTouches){
+    event = e.targetTouches[0]
+  }
+  if (shouldTransition.value) {
+    setTimeout(() => {
+      shouldTransition.value = false
+    }, 300)
+  }
+  pointerX.value = event.clientX - cardLeft
+  pointerY.value = event.clientY - cardTop
+
+
+  const posX = (event.clientX - cardLeft) - cardRef.value.offsetWidth / 2;
+  const posY = (event.clientY - cardTop) - cardRef.value.offsetHeight / 2
+  rotateX.value = posY / 10
+  rotateY.value = -posX / 10
+}
+const handleMouseLeave = () => {
+  shouldTransition.value = true
+  rotateX.value = 0
+  rotateY.value = 0
+  setTimeout(() => {
+    shouldTransition.value = false
+  }, 200)
+  isHover.value = false
+}
+</script>
+
+<style scoped lang="scss">
+.card-wrapper {
+  perspective: 750px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  transform: scale(0.8);
+}
+
+.card {
+  --pointer-x: 0;
+  --pointer-y: 0;
+  --img-url:'';
+  --foil-img-url:'';
+  width: 367px;
+  height: 512px;
+  background-image: var(--img-url);
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  transform: rotateX(calc(var(--mouse-y) * 0.1deg)) rotateY(calc(var(--mouse-x) * -0.1deg));
+  transform-origin: center center;
+  border-radius: 15px;
+  will-change: trasform;
+  overflow: hidden;
+
+
+  .shine {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background:  radial-gradient(circle at var(--pointer-x) var(--pointer-y), rgba(255,255,255,0.6) 0, transparent 70%,transparent 100%);
+    transition: opacity 0.2s ease-out;
+    will-change: background;
+  }
+  &:after {
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-image: var(--foil-img-url);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    border-radius: 10px;
+    mask: radial-gradient(circle at var(--pointer-x) var(--pointer-y), #fff 0, transparent,var(--radial-size),transparent 100%);
+    filter: brightness(0.55) contrast(1.5) saturate(1);
+    mix-blend-mode: color-dodge;
+    will-change: mask, background, filter;
+  }
+
+}
+</style>
--- posts/article/可互动宝可梦卡复刻/index.md
+++ posts/article/可互动宝可梦卡复刻/index.md
@@ -1,15 +1,30 @@
 ---
 category: 笔记
 tags:
-  - CSS JavaScript
+  - CSS
+  - JavaScript
 description: 最近偶然发现一个令人惊艳的卡牌效果网站 Poke-Holo，其动态全息效果和3D交互令人印象深刻。出于学习目的，我决定尝试复刻这个效果，以下是实现过程的完整记录。
+cover: https://6c73-lsj97-9giu4cj4abdc0985-1256331827.tcb.qcloud.la/imgs/2025_05/pokemon.svg
 ---
-<script setup>import Read from "@components/Read.vue";</script>
+<script setup>
+import Read from "@components/Read.vue";
+import Card from './components/Card.vue'
+</script>
 
 <ClientOnly>
   <read></read>
 </ClientOnly>
 
-## 可互动宝可梦卡复刻
+<Card/>
 
-最近偶然发现一个令人惊艳的卡牌效果网站 [Poke-Holo](https://poke-holo.simey.me/)，其动态全息效果和3D交互令人印象深刻。出于学习目的，我决定尝试复刻这个效果，以下是实现过程的完整记录。
+
+# 可互动宝可梦卡复刻
+
+
+最近偶然发现一个令人惊艳的卡牌效果网站 [Poke-Holo](https://poke-holo.simey.me/)，其动态交互令人印象深刻。出于学习目的，我决定尝试用vue简单复刻这个效果(主要复刻鼠标交互效果，浮雕等光影效果未复刻)，以下是实现过程的完整记录。
+
+## 一、效果解析
+通过开发者工具分析及视觉观察，总结出主要复刻要点：
+1. 通过监听鼠标`mousemove`事件，计算出鼠标指针在卡片上的位置。
+2. 通过鼠标指针坐标位置计算出卡片X轴和Y轴上的旋转角度。
+3. 通过
