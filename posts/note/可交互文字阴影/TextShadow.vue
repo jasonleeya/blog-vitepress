e6d881daa11a67682bf077950da206837e37a0ae
lsj <lisj@internal.ths.com.cn>
2025-05-14 18:41:38 +0800

    可交互文字阴影

---
+++ posts/note/可交互文字阴影/TextShadow.vue
@@ -0,0 +1,93 @@
+<!--
+ * @Author: 李双江
+ * @Date: 2025/5/14 16:41:46
+ * @LastEditors: 李双江
+ * @LastEditTime: 2025/5/14 16:41:46
+ * @Description:
+-->
+<template>
+  <div class="container"
+       :style="{
+         '--skew': `${skew}deg`,
+         '--scaleY': scaleY,
+         'font-size': isMobile ? '30px' : '80px',
+         'height': isMobile ? '150px' : '300px'
+       }">
+    <div class="text" data-text="可交互文字阴影">
+      可交互文字阴影
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {onMounted, onUnmounted, ref} from "vue";
+import {useIsMobile} from "@hooks/useIsMobile";
+const isMobile = useIsMobile()
+
+const windowWidth = window.innerWidth
+const windowHeight = window.innerHeight
+let mouseX = 0
+let mouseY = 0
+const skew = ref(0)
+const scaleY = ref(0.5)
+
+const handleMouseMove = (e: MouseEvent|TouchEvent) => {
+  const event = e instanceof MouseEvent ? e : e.touches[0]
+  mouseX = event.clientX
+  mouseY = event.clientY
+  skew.value = 180 * (mouseX - windowWidth / 2) / windowWidth
+  if (mouseY > windowHeight / 2) {
+    scaleY.value = -(1 - (mouseY / (windowHeight / 2))) + 0.5
+  } else {
+    scaleY.value = 0.5 - (0.5 * (1 - mouseY / (windowHeight / 2))) * 0.8
+  }
+}
+
+onMounted(()=>{
+  document.addEventListener('mousemove',handleMouseMove)
+  document.addEventListener('touchmove',handleMouseMove)
+})
+
+onUnmounted(()=>{
+  document.removeEventListener('mousemove',handleMouseMove)
+})
+
+</script>
+
+<style scoped lang="scss">
+.container {
+  --skew: 0deg;
+  --scaleY: 0.5;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #CBD2D5;
+  font-weight: bold;
+  overflow: hidden;
+  height: 300px;
+
+  .text {
+    color: #fff;
+    position: relative;
+    z-index: 1;
+    line-height: 0.76;
+    font-family: Arial, sans-serif;
+
+    &:after {
+      content: attr(data-text);
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      left: 0;
+      bottom: 0;
+      color: #000;
+      transform: translate(-2px, -2px) scaleY(var(--scaleY)) skew(var(--skew));
+      transform-origin: 0 100%;
+      z-index: -1;
+      //mask: linear-gradient(rgba(0, 0, 0, 0.2) 0%, #000 100%);
+      filter: blur(2px);
+    }
+  }
+}
+</style>
---
+++ posts/note/可交互文字阴影/index.md
@@ -0,0 +1,23 @@
+---
+category: 笔记
+tags:
+  - CSS
+  - JavaScript
+cover: https://file.lsj97.com/imgs/2025_05/text_shadow.png
+---
+<script setup>
+import Read from "@components/Read.vue";
+import TextShadow from "./TextShadow.vue";
+</script>
+<ClientOnly>
+  <read></read>
+  <TextShadow/>
+</ClientOnly>
+
+<div style="display: none;">
+
+# 可交互文字阴影
+
+</div>
+
+
